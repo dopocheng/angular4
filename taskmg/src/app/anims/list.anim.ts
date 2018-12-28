23664bf7f8848fa3c5e00df4c961c1633cefd2bf
dopochen <747015697@qq.com>
2018-12-28 21:11:21 +0800

    新建项目动画

---
+++ taskmg/src/app/anims/list.anim.ts
@@ -0,0 +1,16 @@
+import { trigger, transition, style, animate, query, stagger} from '@angular/animations';
+
+export const listAnimation = trigger('listAnim', [
+    transition('* => *', [                                                    // :enter 或 'void => *'
+        query(':enter', style({opacity: 0}),{ optional: true }),
+        query(':enter', stagger(100,[
+            animate('1s', style({opacity: 1}))
+        ]),{ optional: true }),
+        query(':leave', style({opacity: 1}), { optional: true }),
+        query(':leave', stagger(100,[
+            animate('1s', style({opacity: 0}))
+        ]), { optional: true })
+    ]),
+    
+    
+])
--- taskmg/src/app/project/project-list/project-list.component.ts
+++ taskmg/src/app/project/project-list/project-list.component.ts
@@ -6,12 +6,14 @@ import { InviteComponent } from '../invite/invite.component';
 import { ConfirmDialogComponent } from '../../shared/confirm-dialog/confirm-dialog.component';
 import { state } from '@angular/animations';
 import { slideToRight } from '../../anims/router.anim';
+import { listAnimation } from '../../anims/list.anim';
+import { projection } from '@angular/core/src/render3/instructions';
 
 @Component({
   selector: 'app-project-list',
   templateUrl: './project-list.component.html',
   styleUrls: ['./project-list.component.scss'],
-  animations: [slideToRight]
+  // animations: [slideToRight, listAnimation]
 })
 export class ProjectListComponent implements OnInit {
   @HostBinding('@routeAnim') state;
@@ -19,11 +21,13 @@ export class ProjectListComponent implements OnInit {
   // 测试
   projects = [
     {
+      "id" : 1,
       "name": "企业平台",
       "desc": "这是一个企业内部项目",
       "coverImg": "assets/img/covers/0.jpg"
     },
     {
+      "id" : 2,
       "name": "企业平台",
       "desc": "这是一个企业内部项目",
       "coverImg": "assets/img/covers/1.jpg"
@@ -49,12 +53,18 @@ export class ProjectListComponent implements OnInit {
     // 需要得到令牌 （new-project） data {data: 'this is my data sent'}
     const dialogRef = this.dialog.open(NewProjectComponent, {data: {title: '新建项目'}});
     // 接受返回来的数据
-    // dialogRef.afterClosed().subscribe(result => console.error(result));
+    dialogRef.afterClosed().subscribe(result => {
+      console.error(result);
+      this.projects = [...this.projects,{id: 3, name:'一个新项目', desc: '这是一个新项目', coverImg: 'assets/img/covers/8.jpg'}]
+    });
   }
 
-  launchDelDialog() {
+  launchDelDialog(project) {
    const dialogRef = this.dialog.open(ConfirmDialogComponent,{data:{title: '删除任务',content: '你确认要删除么？'}});
-    dialogRef.afterClosed().subscribe(res => {console.log(res)})
+    dialogRef.afterClosed().subscribe(res => {
+      console.log(res);
+      this.projects = this.projects.filter(p => p.id !== project.id);
+    })
   }
 
 }
